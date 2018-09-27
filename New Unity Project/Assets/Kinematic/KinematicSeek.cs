2f61a5032a9b45404acb25e058c0b9d06145ab44
Vladislav Bulantsev <vladislavbv96@gmail.com>
2018-09-27 14:28:33 +0200

    todos

---
+++ New Unity Project/Assets/Kinematic/KinematicSeek.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KinematicSeek : MonoBehaviour {
+
+	Move move;
+
+	// Use this for initialization
+	void Start () {
+		move = GetComponent<Move>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        // TODO 5: Set movement velocity to max speed in the direction of the target
+
+        move.SetMovementVelocity(move.mov_velocity);
+
+        // Remember to enable this component in the inspector
+	}
+}
